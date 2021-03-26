876d65c49e092795e84c5021575bed4fa13eeb19
Chung-Hsuan, Tsai <chtsai.cs08g@nctu.edu.tw>
2021-03-26 17:22:58 +0800

    Update at 2021-03-26 17:22:58

--- ptAttendance.user.js
+++ ptAttendance.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  過濾兼任差勤及服務管理系統之請核列表，將當月以前的資訊加上刪除線。
-// @author       ch-tsai setsal
+// @author       iasthc setsal
 // @include      https://pt-attendance.nctu.edu.tw/index.php     
 // @grant        none
 // ==/UserScript==
@@ -15,9 +15,15 @@
     for (var i = 0; i < ptLists.item(0).childNodes.length; i++) {
         var ptDate = ptLists.item(0).childNodes[i].childNodes[14].innerText;
         var ptDay = new Date(ptDate + 'T00:00:00');
+        var diffMonths = ptDay.getMonth() - day.getMonth() + (12 * (ptDay.getFullYear() - day.getFullYear()));
         if (day > ptDay) {
             for (var j = 0; j < ptLists.item(0).childNodes[i].childNodes.length; j++) {
                 ptLists.item(0).childNodes[i].childNodes[j].innerHTML = '<td class="text-right align-middle"><span style="color:grey;"><del>' + ptLists.item(0).childNodes[i].childNodes[j].innerText + '</del></span></td>';
+            }
+        }
+        if (diffMonths == 0) {
+            for (var j = 0; j < ptLists.item(0).childNodes[i].childNodes.length; j++) {
+                ptLists.item(0).childNodes[i].childNodes[j].innerHTML = '<td class="text-right align-middle"><span style="color:red;">' + ptLists.item(0).childNodes[i].childNodes[j].innerText + '</span></td>';
             }
         }
     }
